a4c00e227c6579ce6672adbb6cc76e68484e7569
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-06-05 15:53:27 -0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+# Create a file upload widget
+uploaded_file = st.file_uploader("Upload a file", type=['abf'])
 
-uploaded_file = st.file_uploader(label='Upload ABF files', accept_multiple_files=True,  type=['abf'])
-st.write(uploaded_file)
+# Check if a file is uploaded
+if uploaded_file is not None:
+    # Get the file name
+    file_name = uploaded_file.name
+    
+    # Display the file name as a clickable link
+    file_link = f"[{file_name}]({uploaded_file.name})"
+    st.markdown(file_link, unsafe_allow_html=True)
+    
+    # Trigger an event when the file name is clicked
+    if st.button("Click here"):
+        # Perform your desired action
+        st.write("File name clicked!")
