fb71d264311a33971613ef5fc50e8e642c12c7f6
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2023-06-06 01:26:54 -0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -8,7 +8,7 @@ import subthresh_features as sbth
 import qc_features as qc
 
 
-st.title("Ephys feature analysis")
+st.set_page_config(page_title="Ephys feature analysis")
 
 save_directory = "./uploads"
 
